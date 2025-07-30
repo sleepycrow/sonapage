d01d4c52891642274e5565cd3c6eba44edda0b88
sleepycrow <sleepycrow@posteo.net>
2025-07-31 01:52:23 +0200

    Add FursonaSchema types

---
+++ src/interfaces/FursonaSchema.ts
@@ -0,0 +1,33 @@
+/**
+ * TS types for Fursona Schema v1.3 (as of commit cd76477, from 2025-02-11)
+ * https://github.com/pyrox0/fursona-schema/
+ */
+
+export interface FursonaSchema {
+	sonas: Sona[];
+}
+
+export interface Sona {
+	name: string;
+	species: string;
+	description: string;
+	pronouns?: string;
+	gender?: string;
+	avatar?: string;
+	avatarAlt?: string;
+	avatarAttribution?: string;
+	ref?: string;
+	refAlt?: string;
+	refAttribution?: string;
+	age?: number;
+	birthdate?: string;
+	colors?: string[];
+	gallery?: GalleryItem[];
+}
+
+export interface GalleryItem {
+	image: string;
+	imageAlt?: string;
+	imageAttribution?: string;
+	description?: string;
+}
