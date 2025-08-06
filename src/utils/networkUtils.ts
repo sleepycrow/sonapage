70b8c756b732a8d058be1b573257252a3cd7114f
sleepycrow <sleepycrow@posteo.net>
2025-08-06 23:50:54 +0200

    Improve error handling in fetch method

--- src/utils/networkUtils.ts
+++ src/utils/networkUtils.ts
@@ -3,7 +3,7 @@ import type { FursonaSchema } from "@/interfaces/FursonaSchema";
 const URI_HOST_REGEX = /^(?:https?:\/\/)?((?:[\p{L}0-9-]{2,})(?:\.[\p{L}0-9-]{2,})+)/ui;
 
 export function extractHost(uri: string): string | null {
-	const matches = URI_HOST_REGEX.exec(uri || '');
+	const matches = URI_HOST_REGEX.exec(uri.trim() || '');
 	return (matches && matches?.length > 1)
 		? matches[1]
 		: null;
@@ -33,12 +33,18 @@ export async function loadFursonaInfo(host: string): Promise<FursonaSchema> {
 	const urls = getFursonaInfoUrls(host);
 	for (let url of urls) {
 		const resp = await fetch(url);
-		if (resp.ok)
+		if (resp.ok) {
 			return await resp.json();
-		else if (resp.status === 404)
+		} else if (resp.status === 404) {
 			continue;
-		else
-			throw new Error(`${resp.status} - ${resp.statusText}`);
+		} else {
+			let body;
+			try {
+				body = await resp.text();
+			} catch (_e) {}
+
+			throw new Error(`${resp.status} - ${resp.statusText}` + (body ? ` (${body})` : ''));
+		}
 	}
 
 	throw new Error('404 - Not Found');
